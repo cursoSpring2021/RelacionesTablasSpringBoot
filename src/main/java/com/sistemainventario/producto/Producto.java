e003a5af469934abb23fc3130696c4e91bb6ae34
cursoSpring2021 <dguerra723@unab.edu.co>
2022-05-18 14:19:23 -0500

    Se modifico el productoDetalles y producto_formulario

--- src/main/java/com/sistemainventario/producto/Producto.java
+++ src/main/java/com/sistemainventario/producto/Producto.java
@@ -3,75 +3,100 @@ package com.sistemainventario.producto;
 import com.sistemainventario.categoria.Categoria;
 
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "productos")
 public class Producto {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	@Column(length = 128, nullable = false, unique = true)
-	private String nombre;
-	private float precio;
-	@ManyToOne
-	@JoinColumn(name = "categoria_id")
-	private Categoria categoria;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 128, nullable = false, unique = true)
+    private String nombre;
+    private float precio;
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
 
-	public Producto() {
-		super();
-	}
+    @OneToMany(mappedBy = "producto", cascade = CascadeType.ALL)
+    private List<ProductoDetalles> detalles = new ArrayList<>();
 
-	public Producto(String nombre, float precio, Categoria categoria) {
-		super();
-		this.nombre = nombre;
-		this.precio = precio;
-		this.categoria = categoria;
-	}
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 
-	public Producto(Integer id, String nombre, float precio, Categoria categoria) {
-		super();
-		this.id = id;
-		this.nombre = nombre;
-		this.precio = precio;
-		this.categoria = categoria;
-	}
+    public void añadirDetalles(String nombre, String valor) {
+        this.detalles.add(new ProductoDetalles(nombre, valor, this));
 
-	public Producto(String nombre) {
-		super();
-		this.nombre = nombre;
-	}
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public List<ProductoDetalles> getDetalles() {
+        return detalles;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setDetalles(List<ProductoDetalles> detalles) {
+        this.detalles = detalles;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public void setDetalle(Integer id, String nombre, String valor){
+       this.detalles.add(new ProductoDetalles(id,nombre,valor,this));
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    }
 
-	public float getPrecio() {
-		return precio;
-	}
+    public Producto() {
+        super();
+    }
 
-	public void setPrecio(float precio) {
-		this.precio = precio;
-	}
+    public Producto(String nombre, float precio, Categoria categoria) {
+        super();
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
 
-	public Categoria getCategoria() {
-		return categoria;
-	}
+    public Producto(Integer id, String nombre, float precio, Categoria categoria) {
+        super();
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
 
-	public void setCategoria(Categoria categoria) {
-		this.categoria = categoria;
-	}
+    public Producto(String nombre) {
+        super();
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
 
 }
--- src/main/java/com/sistemainventario/producto/ProductoController.java
+++ src/main/java/com/sistemainventario/producto/ProductoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -31,7 +32,18 @@ public class ProductoController {
     }
 
     @PostMapping("/productos/guardar")
-    public String guardarProducto(Producto producto) {
+    public String guardarProducto(Producto producto, HttpServletRequest request) {
+        String[] detallesIDs = request.getParameterValues("detallesIDs");
+        String[] detallesNombres = request.getParameterValues("detallesNombre");
+        String[] detallesValores = request.getParameterValues("detallesValor");
+        for (int i = 0; i < detallesNombres.length; i++) {
+           if (detallesIDs != null && detallesIDs.length > 0){
+            producto.setDetalle(Integer.valueOf(detallesIDs[i]),detallesNombres[i],detallesValores[i]);
+
+           }else {
+               producto.añadirDetalles(detallesNombres[i],detallesValores[i]);
+           }
+        }
         productoRepository.save(producto);
         return "redirect:/";
 
@@ -49,13 +61,13 @@ public class ProductoController {
         Producto producto = productoRepository.findById(id).get();
         model.addAttribute("producto", producto);
         List<Categoria> listaCategorias = categoriaRepository.findAll();
-        model.addAttribute("listaCategorias",listaCategorias);
+        model.addAttribute("listaCategorias", listaCategorias);
         return "producto_formulario";
     }
 
     @GetMapping("/productos/eliminar/{id}")
 
-    public String eliminarProducto(@PathVariable Integer id, Model model){
+    public String eliminarProducto(@PathVariable Integer id, Model model) {
         productoRepository.deleteById(id);
         return "redirect:/productos";
     }
